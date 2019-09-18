979dca1600bdc71e9fba5fdd58d9bb74354efdaa
gaoxiangyu <you@example.com>
2019-09-18 11:21:12 +0800

    适配

--- lottery/postcss.config.js
+++ lottery/postcss.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   plugins: {
-    autoprefixer: {}
-  }
+    autoprefixer: {},
+    "postcss-px2rem-exclude": {
+        remUnit: 75,  //750的设计稿
+        exclude: /node_modules|folder_name/i
+    }
+}
 }
--- lottery/src/views/Home.vue
+++ lottery/src/views/Home.vue
@@ -1,18 +1,50 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="ho_header">
+        <div class="hd_top">
+            <p class="hd_hello">上午好！朋友们</p>
+            <div>
+              <img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1540139703196&di=e4cd33a4c426a158199be6b7b9ece9ac&imgtype=0&src=http%3A%2F%2Fbpic.588ku.com%2Felement_origin_min_pic%2F01%2F31%2F87%2F96573b585a7c9c4.jpg" alt="">
+              <p>用户名</p>
+            </div>
+        </div>
+        <div class="hd_bo">
+            <div class="bo_money"></div>
+            <div class="bo_inte"></div>
+        </div>
+    </div>
+    <div class="ho_three">
+
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
   }
 }
 </script>
+<style scoped>
+*{margin:0;padding:0;}
+
+.home{
+      height: 100%;
+     background-color: #077552;
+}
+.ho_header{
+  text-align: center;
+  background-color: rgb(8, 83, 60);
+  }
+.ho_header .hd_hello{
+    caret-color: #f5f5f5;
+    color: #f5f5f5;
+  }
+.ho_header img{
+      height: 70px;
+    width: 70px;
+    border-radius: 50%;
+}
+</style>
