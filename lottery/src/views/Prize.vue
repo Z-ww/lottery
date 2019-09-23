3a0c5ac7b817cdaea0ca1e2ff1f885204656af4c
zhaolinfei <1103905129@qqcom>
2019-09-23 11:37:05 +0800

    飞

--- lottery/src/views/Prize.vue
+++ lottery/src/views/Prize.vue
@@ -89,7 +89,6 @@ export default {
     .prize{
         width: 100%;
         height: 100%;
-        background: white;
     }
     .banner{
         width: 100%;
@@ -128,7 +127,6 @@ export default {
         flex: 1 1 auto;
         text-align: center;
         line-height: 60px;
-        margin-bottom: 0px;
     }
     .fale{
         position: absolute;
@@ -180,7 +178,7 @@ export default {
          left: 0;
          background: rgba(0,0,0,.4);
          width: 100%;
-         height: 100%;
+         height: 160%;
          transition: .35s;
      }
      .zezo>div{
@@ -205,7 +203,6 @@ export default {
         
     }
     .zezo_pe{
-    	background: white;
         padding: 16px;
         overflow: hidden;
     }
@@ -216,7 +213,5 @@ export default {
         line-height: 46px;
         text-align: center;
         color: #4caf50;
-        font-weight: bold;
-        font-size: 24px;
     }
 </style>
